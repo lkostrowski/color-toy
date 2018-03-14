ca6d9d044f4dac5ba844e75084df56743b23a666
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2018-03-15 00:35:22 +0100

    Added custom font

--- src/baseStyles.js
+++ src/baseStyles.js
@@ -1,12 +1,22 @@
 import {css} from 'glamor';
 
 css.global('html,body', {
-    fontFamily: 'sans-serif',
+    fontFamily: 'Montserrat, sans-serif',
     margin: 0,
     padding: 0,
-    boxSizing: 'border-box'
+    boxSizing: 'border-box',
 });
 
 css.global('*', {
-    boxSizing: 'inherit'
+    boxSizing: 'inherit',
+    fontWeight: '300',
+    fontFamily: 'inherit'
 });
+
+css.insert(`
+  @font-face {
+    font-family: 'Montserrat';
+    src: url('/Montserrat-Light.ttf');
+    font-weight: 300;
+  }
+`);
