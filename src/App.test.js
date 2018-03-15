29e62edbc7064cfa1d9aca81db5bb8da16f1fc7f
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2018-03-15 22:08:46 +0100

    Tests setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import {shallow} from 'enzyme';
+import {App} from './App';
 
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
-    ReactDOM.unmountComponentAtNode(div);
+    const noop = () => {
+    };
+    shallow(<App fetchColors={noop}/>);
+
+});
+
+it('run fetch colors action when mounted', (done) => {
+    const fetchColorsMock = () => {
+        done();
+    };
+    shallow(<App fetchColors={fetchColorsMock}/>);
+
 });
